bff8455519e4838ab906483fdd1ae33ba0588c7a
John <jegonzalezesparragoza@gmail.com>
2023-05-25 22:03:42 -0500

    feat: :sparkles: Taller 2
    Clase padre del proyecto => Animal

--- index.js
+++ index.js
@@ -1,70 +1,55 @@
-class Persona {
-    nombre
+class Animal {
+    nombre 
     edad
-    sexo
-    constructor(nombre, edad, sexo){
-        this.nombre = nombre;
+    constructor(nombre,edad){
+        this.nombre =nombre;
         this.edad = edad;
-        this.sexo = sexo;
     }
-    saludar(){
-        let mensaje = document.querySelector("#messages");
-        mensaje.textContent =`Hola mi nombre es ${this.nombre}, tengo ${this.edad} y mi sexo es ${this.sexo}`
+    hacerSonido(){
+        document.querySelector("#messages").textContent = "";
+        const mensaje = document.querySelector("#messages");
+        mensaje.textContent =`El animal llamado ${this.nombre}, con edad ${this.edad} está haciendo un sonido`;
     }
-    static esMayorDeEdad(edad){
-        if (edad >= 18){
-            document.querySelector("#mayor").value = "Sí";
-            return console.log(true); 
-        }else{
-            document.querySelector("#mayor").value = "No";
-            return console.log(false);
-        }
+};
 
+
+class Perro extends Animal{
+    raza
+    constructor(nombre,edad,raza){
+        super(nombre,edad)
+        this.raza = raza;
+    }
+    moverCola(){
+        document.querySelector("#messages").textContent = "";
+        const mensaje = document.querySelector("#messages");
+        mensaje.textContent += `El perro ${this.nombre}, con edad ${this.edad} de raza ${this.raza} está moviendo la cola`
     }
 }
-// Cambiar formulario
+
 document.querySelector("#tipo").addEventListener("change",(e)=>{
-    const carrera = document.querySelector("#carreraArea")
-    if(e.target.value === "Estudiante"){
-        carrera.innerHTML =`
+    const razaArea = document.querySelector("#razaArea");
+    if(e.target.value === "Perro" ){
+        razaArea.innerHTML = `
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="carrera" placeholder="carrera">
-            <label for="floatingPassword">Carrera</label>
+            <input type="text" class="form-control" id="raza" placeholder="raza">
+            <label for="floatingInput">Raza</label>
         </div>
-        `
-    }else{
-        carrera.innerHTML ="";
-    }
+        `  
+    }else razaArea.innerHTML ="";
 });
-
-
-class Estudiante extends Persona{
-    carrera
-    constructor(nombre,edad,sexo,carrera){
-        super(nombre,edad,sexo)
-        this.carrera = carrera;
-    }
-    estudiar(){
-        let mensaje = document.querySelector("#messages");
-        mensaje.textContent += `Hola mi nombre es ${this.nombre}, tengo ${this.edad},mi sexo es ${this.sexo} y estoy estudiando ${this.carrera}`
-    }
-}
 
 document.querySelector(".btn-dark").addEventListener("click",()=>{
     const nombre = document.querySelector("#nombre").value;
-    const edad = Number(document.querySelector("#edad").value);
-    const sexo = document.querySelector("#sexo").value;
+    const edad = document.querySelector("#edad").value;
     const tipo = document.querySelector("#tipo").value;
     
 
-    if(tipo === "Estudiante"){
-        const carrera = document.querySelector("#carrera").value;
-        let estudiante1 = new Estudiante (nombre,edad,sexo,carrera);
-        estudiante1.estudiar();
-        Estudiante.esMayorDeEdad(edad);
+    if(tipo === "Perro"){
+        const raza = document.querySelector("#raza").value;
+        let perro1 = new Perro (nombre,edad,raza);
+        perro1.moverCola();
     }else{
-        let persona1 = new Persona (nombre,edad,sexo);
-        persona1.saludar();
-        Persona.esMayorDeEdad(edad);
+        let animal1 = new Animal (nombre,edad);
+        animal1.hacerSonido();
     }
 });
