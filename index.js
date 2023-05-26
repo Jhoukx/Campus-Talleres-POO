295a261c7309702d9267a3fd3c175257cefe37e4
John González <jegonzalezesparragoza@gmail.com>
2023-05-26 13:12:15 -0500

    feat: :sparkles: Taller 3
    Clase padre => Figura

--- index.js
+++ index.js
@@ -1,55 +1,108 @@
-class Animal {
-    nombre 
-    edad
-    constructor(nombre,edad){
-        this.nombre =nombre;
-        this.edad = edad;
+class Figura {
+    color 
+    area
+    constructor({color= "blanco", area= 0}){
+        this.color = color;
+        this.area = area;
     }
-    hacerSonido(){
+    get color (){
+        return this.color;
+    }
+    get area (){
+        return this.area;
+    }
+    calcularArea(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.textContent =`El animal llamado ${this.nombre}, con edad ${this.edad} está haciendo un sonido`;
+        mensaje.textContent =`La figura de color ${this.color}, tiene un area de ${this.area}`;
     }
 };
 
 
-class Perro extends Animal{
-    raza
-    constructor(nombre,edad,raza){
-        super(nombre,edad)
-        this.raza = raza;
+class Circulo extends Figura{
+    radio
+    constructor({ color, area, radio }){
+        super({ color, area })
+        this.radio = radio;
     }
-    moverCola(){
+    get radio (){
+        return this.radio;
+    }
+    calcularArea(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.textContent += `El perro ${this.nombre}, con edad ${this.edad} de raza ${this.raza} está moviendo la cola`
+        mensaje.textContent += `El circulo de color ${this.color}, tiene ${Math.floor(Math.PI * this.radio **2)}  de area`
+    }
+}
+
+class Rectangulo extends Figura{
+    largo
+    ancho
+    constructor({color,area,largo, ancho}){
+        super({color,area})
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+    get largo (){
+        return this.largo;
+    }
+    get ancho (){
+        return this.ancho;
+    }
+    calcularArea(){
+        document.querySelector("#messages").textContent = "";
+        const mensaje = document.querySelector("#messages");
+        mensaje.textContent += `El area del rectangulo es de ${this.largo * this.ancho}`
     }
 }
 
 document.querySelector("#tipo").addEventListener("change",(e)=>{
-    const razaArea = document.querySelector("#razaArea");
-    if(e.target.value === "Perro" ){
-        razaArea.innerHTML = `
+    if(e.target.value === "Circulo" ){
+        document.querySelector("#rectanguloArea").innerHTML = "";
+        const cirArea = document.querySelector("#circuloArea");
+        cirArea.innerHTML = `
         <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="raza" placeholder="raza">
-            <label for="floatingInput">Raza</label>
+            <input type="number" class="form-control" id="radio" placeholder="radio">
+            <label for="floatingPassword">Radio</label>
         </div>
         `  
-    }else razaArea.innerHTML ="";
+    }else if (e.target.value === "Rectangulo"){
+        document.querySelector("#circuloArea").innerHTML="";
+        const rectArea = document.querySelector("#rectanguloArea");
+        rectArea.innerHTML = `
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="largo" placeholder="largo">
+            <label for="floatingPassword">Largo</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="ancho" placeholder="ancho">
+            <label for="floatingPassword">Ancho</label>
+        </div>
+        `
+    }else{
+        document.querySelector("#circuloArea").innerHTML = "";
+        document.querySelector("#rectanguloArea").innerHTML = "";
+    }
 });
 
 document.querySelector(".btn-dark").addEventListener("click",()=>{
-    const nombre = document.querySelector("#nombre").value;
-    const edad = document.querySelector("#edad").value;
+    const color = document.querySelector("#color").value;
+    const area = document.querySelector("#area").value;
     const tipo = document.querySelector("#tipo").value;
     
 
-    if(tipo === "Perro"){
-        const raza = document.querySelector("#raza").value;
-        let perro1 = new Perro (nombre,edad,raza);
-        perro1.moverCola();
-    }else{
-        let animal1 = new Animal (nombre,edad);
-        animal1.hacerSonido();
+    if(tipo === "Circulo"){
+        const radio = Number(document.querySelector("#radio").value);
+        let circulo1 = new Circulo({ color, area, radio });
+        circulo1.calcularArea();
+    }else if(tipo === "Rectangulo"){
+        const largo = document.querySelector("#largo").value;
+        const ancho = document.querySelector("#ancho").value;
+        let rectangulo1 = new Rectangulo({color,area,largo,ancho})
+        rectangulo1.calcularArea();
+    }
+    else{
+        let figura1 = new Figura({ color, area });
+        figura1.calcularArea();
     }
 });
