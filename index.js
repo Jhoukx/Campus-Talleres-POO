dcbc4098a016c7d90274cd31810ad06d5bee2967
Jhoukx <127460605+Jhoukx@users.noreply.github.com>
2023-05-27 17:54:24 -0500

    Feat : sparkles Taller 4
    La clase padre del ejercicio es => Vehiculo

--- index.js
+++ index.js
@@ -1,108 +1,73 @@
-class Figura {
-    color 
-    area
-    constructor({color= "blanco", area= 0}){
-        this.color = color;
-        this.area = area;
+class Vehiculo {
+    marca 
+    modelo
+    velocidad
+    constructor(marca,modelo,velocidad){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
     }
-    get color (){
-        return this.color;
+    get marca (){
+        return this.marca
     }
-    get area (){
-        return this.area;
+    get modelo (){
+        return this.modelo
     }
-    calcularArea(){
+    get velocidad (){
+        return this.velocidad
+    }
+    acelerar(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.textContent =`La figura de color ${this.color}, tiene un area de ${this.area}`;
+        mensaje.innerHTML = `Ahora la velocidad del vehiculo es de ${this.velocidad + 10} Km `
+    }
+    static convertirKmHEnMph(){
+
     }
 };
 
 
-class Circulo extends Figura{
-    radio
-    constructor({ color, area, radio }){
-        super({ color, area })
-        this.radio = radio;
+class Coche extends Vehiculo{
+    combustible
+    constructor(marca,modelo,velocidad,combustible){
+        super(marca,modelo,velocidad)
+        this.combustible = combustible;
     }
-    get radio (){
-        return this.radio;
-    }
-    calcularArea(){
+    acelerar(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.textContent += `El circulo de color ${this.color}, tiene ${Math.floor(Math.PI * this.radio **2)}  de area`
-    }
-}
-
-class Rectangulo extends Figura{
-    largo
-    ancho
-    constructor({color,area,largo, ancho}){
-        super({color,area})
-        this.largo = largo;
-        this.ancho = ancho;
-    }
-    get largo (){
-        return this.largo;
-    }
-    get ancho (){
-        return this.ancho;
-    }
-    calcularArea(){
-        document.querySelector("#messages").textContent = "";
-        const mensaje = document.querySelector("#messages");
-        mensaje.textContent += `El area del rectangulo es de ${this.largo * this.ancho}`
+        mensaje.innerHTML = `Ahora la velocidad del vehiculo es de ${this.velocidad + 20} Km `
     }
 }
 
 document.querySelector("#tipo").addEventListener("change",(e)=>{
-    if(e.target.value === "Circulo" ){
-        document.querySelector("#rectanguloArea").innerHTML = "";
-        const cirArea = document.querySelector("#circuloArea");
-        cirArea.innerHTML = `
+    if(e.target.value === "Coche" ){
+        document.querySelector("#cocheArea").innerHTML = "";
+        const coche = document.querySelector("#cocheArea");
+        coche.innerHTML = `
         <div class="form-floating mb-3">
-            <input type="number" class="form-control" id="radio" placeholder="radio">
-            <label for="floatingPassword">Radio</label>
+            <input type="number" class="form-control" id="combustible" placeholder="Combustible">
+            <label for="floatingPassword">Combustible</label>
         </div>
         `  
-    }else if (e.target.value === "Rectangulo"){
-        document.querySelector("#circuloArea").innerHTML="";
-        const rectArea = document.querySelector("#rectanguloArea");
-        rectArea.innerHTML = `
-        <div class="form-floating mb-3">
-            <input type="number" class="form-control" id="largo" placeholder="largo">
-            <label for="floatingPassword">Largo</label>
-        </div>
-        <div class="form-floating mb-3">
-            <input type="number" class="form-control" id="ancho" placeholder="ancho">
-            <label for="floatingPassword">Ancho</label>
-        </div>
-        `
     }else{
-        document.querySelector("#circuloArea").innerHTML = "";
-        document.querySelector("#rectanguloArea").innerHTML = "";
+        document.querySelector("#cocheArea").innerHTML = "";
     }
 });
 
 document.querySelector(".btn-dark").addEventListener("click",()=>{
-    const color = document.querySelector("#color").value;
-    const area = document.querySelector("#area").value;
+    const marca = document.querySelector("#marca").value;
+    const modelo = document.querySelector("#modelo").value;
+    const velocidad = Number(document.querySelector("#velocidad").value);
     const tipo = document.querySelector("#tipo").value;
     
 
-    if(tipo === "Circulo"){
-        const radio = Number(document.querySelector("#radio").value);
-        let circulo1 = new Circulo({ color, area, radio });
-        circulo1.calcularArea();
-    }else if(tipo === "Rectangulo"){
-        const largo = document.querySelector("#largo").value;
-        const ancho = document.querySelector("#ancho").value;
-        let rectangulo1 = new Rectangulo({color,area,largo,ancho})
-        rectangulo1.calcularArea();
-    }
-    else{
-        let figura1 = new Figura({ color, area });
-        figura1.calcularArea();
+    if(tipo === "Coche"){
+        const combustible = Number(document.querySelector("#combustible").value);
+        let Coche1 = new Coche(marca,modelo,velocidad,combustible);
+        Coche1.acelerar();
+    }else{
+        let vehiculo1 = new Vehiculo(marca,modelo,velocidad);
+        vehiculo1.acelerar();
     }
 });
