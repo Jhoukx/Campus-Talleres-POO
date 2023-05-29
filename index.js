bbbf2bcdf09546cdc43153f66e6c7dc348fa592d
John González <jegonzalezesparragoza@gmail.com>
2023-05-29 11:43:57 -0500

    feat: :sparkles: Taller-5
    Clase padre => Empleado

--- index.js
+++ index.js
@@ -1,73 +1,78 @@
-class Vehiculo {
-    marca 
-    modelo
-    velocidad
-    constructor(marca,modelo,velocidad){
-        this.marca = marca;
-        this.modelo = modelo;
-        this.velocidad = velocidad;
+class Empleado {
+    nombre
+    edad
+    sueldo
+    static id =0
+    constructor(nombre,edad,sueldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+        ++Empleado.id
     }
-    get marca (){
-        return this.marca
+    get nombre (){
+        return this.color;
     }
-    get modelo (){
-        return this.modelo
+    get edad (){
+        return this.area;
     }
-    get velocidad (){
-        return this.velocidad
+    get sueldo (){
+        return this.sueldo;
     }
-    acelerar(){
+    calcularSalarioAnual(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.innerHTML = `Ahora la velocidad del vehiculo es de ${this.velocidad + 10} Km `
+        return mensaje.textContent =`El salario anual es de ${this.sueldo * 12}$`;
     }
-    static convertirKmHEnMph(){
-
+    static generarIdEmpleado (){
+        return console.log(Empleado.id);
     }
 };
 
 
-class Coche extends Vehiculo{
-    combustible
-    constructor(marca,modelo,velocidad,combustible){
-        super(marca,modelo,velocidad)
-        this.combustible = combustible;
+class Gerente extends Empleado{
+    departamento
+    constructor(nombre,edad,sueldo,departamento){
+        super(nombre,edad,sueldo)
+        this.departamento = departamento;
     }
-    acelerar(){
+    get departamento (){
+        return this.departamento;
+    }
+    calcularSalarioAnual(){
         document.querySelector("#messages").textContent = "";
         const mensaje = document.querySelector("#messages");
-        mensaje.innerHTML = `Ahora la velocidad del vehiculo es de ${this.velocidad + 20} Km `
+        return mensaje.textContent += `El sueldo del  Gerente del departamento ${this.departamento} es de ${((this.sueldo*0.1)+this.sueldo)*12}$ `
     }
 }
 
 document.querySelector("#tipo").addEventListener("change",(e)=>{
-    if(e.target.value === "Coche" ){
-        document.querySelector("#cocheArea").innerHTML = "";
-        const coche = document.querySelector("#cocheArea");
-        coche.innerHTML = `
+    if(e.target.value === "Gerente" ){
+        document.querySelector("#gerenteArea").innerHTML = "";
+        const gerArea = document.querySelector("#gerenteArea");
+        gerArea.innerHTML = `
         <div class="form-floating mb-3">
-            <input type="number" class="form-control" id="combustible" placeholder="Combustible">
-            <label for="floatingPassword">Combustible</label>
+            <input type="text" class="form-control" id="departamento" placeholder="departamento">
+            <label for="floatingInput">Departamento</label>
         </div>
         `  
     }else{
-        document.querySelector("#cocheArea").innerHTML = "";
+        document.querySelector("#gerenteArea").innerHTML = "";
     }
 });
 
 document.querySelector(".btn-dark").addEventListener("click",()=>{
-    const marca = document.querySelector("#marca").value;
-    const modelo = document.querySelector("#modelo").value;
-    const velocidad = Number(document.querySelector("#velocidad").value);
+    const nombre = document.querySelector("#nombre").value;
+    const edad = document.querySelector("#edad").value;
+    const sueldo = Number(document.querySelector("#sueldo").value);
     const tipo = document.querySelector("#tipo").value;
-    
 
-    if(tipo === "Coche"){
-        const combustible = Number(document.querySelector("#combustible").value);
-        let Coche1 = new Coche(marca,modelo,velocidad,combustible);
-        Coche1.acelerar();
+    if(tipo === "Gerente"){
+        const departamento= document.querySelector("#departamento").value;
+        const gerente1 = new Gerente(nombre,edad,sueldo,departamento);
+        gerente1.calcularSalarioAnual();
     }else{
-        let vehiculo1 = new Vehiculo(marca,modelo,velocidad);
-        vehiculo1.acelerar();
+        let empleado1 = new Empleado(nombre,edad,sueldo);
+        empleado1.calcularSalarioAnual();
+        Empleado.generarIdEmpleado();
     }
 });
